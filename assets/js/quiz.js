c4493ded57f56884a7b7d9ce8d3fc67b8e152819
mcalik6 <mcalik6@gmail.com>
2023-03-28 21:05:00 +0000

    score board is not working

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,16 +1,34 @@
 let quizanswers = document.getElementById("quizanswers");
 let scoreDisplay = document.getElementById("score");
 
-quizanswers.addEventListener("submit",function(event)){
+quizanswers.addEventListener("submit", function(event) {
     event.preventDefault();
-let score=0;
+    let score = 0;
 
-let a1Answer =document.querySelector("input[name=a1]:checked")
-if (a1Answer && a1Answer.value === "2"){
-    score++;
-}
+    let a1Answer = document.querySelector("input[name=a1]:checked")
+    if (a1Answer && a1Answer.value === "2"){
+        score++;
+    }
 
-scoreDisplay.innerHTML = `Your score:${score}`;
+    let a2Answer = document.querySelector("input[name=a2]:checked")
+    if (a2Answer && a2Answer.value === "2"){
+        score++;
+    }
 
+    let a3Answer = document.querySelector("input[name=a3]:checked")
+    if (a3Answer && a3Answer.value === "2"){
+        score++;
+    }
 
-}
+    let a4Answer = document.querySelector("input[name=a4]:checked")
+    if (a4Answer && a4Answer.value === "1"){
+        score++;
+    }
+
+    let a5Answer = document.querySelector("input[name=a5]:checked")
+    if (a5Answer && a5Answer.value === "0"){
+        score++;
+    }
+
+    scoreDisplay.innerHTML = `Your score : ${score}`;
+});
