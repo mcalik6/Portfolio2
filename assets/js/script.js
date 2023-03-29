0fd79d8b052ace1a8aec7ce0717df67d6df0cfeb
mcalik6 <mcalik6@gmail.com>
2023-03-29 06:48:25 +0000

    Ive made it simplier and still not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
-const urlParams = new URLSearchParams(window.location.search);
-const name = urlParams.get('name');
+const quizForm = document.querySelector(`#quizanswers`);
+const scoreDisplay = document.querySelector(`#score`);
 
-const nameDisplay = document.createElement('h1');
-nameDisplay.textContent = `Hello,${name || 'New Player'}!`;
-document.body.appendChild(nameDisplay);
+const answers = [`2`, `2`, `0`, `1`];
+
+quizForm.addEventListener(`submit`, (event) => {
+    event.preventDefault()
+    let.score = 0;
+
+
+
+const userAnswers = [
+    quizForm.elements[`a1`].value,
+    quizForm.elements[`a2`].value,
+    quizForm.elements[`a3`].value,
+    quizForm.elements[`a4`].value,
+];
+
+userAnswers.forEach((answer, index) => {
+    if (answer === answers[index]) {
+        score += 1;
+    }
+});
+
+scoreDisplay.innerHTML = `YOure score: ${score}`;
+
+})
