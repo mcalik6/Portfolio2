de10cc3570d819ad9b76dde9bdc8da9fe92de60d
mcalik6 <mcalik6@gmail.com>
2023-03-29 07:46:05 +0000

    adding back contestant name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get('name');
+
+const nameDisplay = document.createElement('h1');
+nameDisplay.textContent = `Hello,${name || 'New Player'}!`;
+document.body.appendChild(nameDisplay);
+
 const quizForm = document.querySelector(`#quizanswers`);
 const scoreDisplay = document.querySelector(`#score`);
 
